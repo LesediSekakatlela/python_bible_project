572b7d0ec0409c596ccf34f205244ff3b4723ff3
Lesedi <leseditumelo32@gmail.com>
2021-06-13 13:43:01 +0200

    whileloop

--- chap8/while.py
+++ chap8/while.py
@@ -1,3 +1,23 @@
+#how loops work
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+
+#using a while loop
+number = 1
+
+while number <= 10:
+    print(number)
+    number = number + 1
+#prints out the number 11
+print(number)
+
+
+
+   
 L = []
 
 while len(L) < 5:
@@ -6,3 +26,5 @@ while len(L) < 5:
 
 print("Sorry list is full")
 print(L)
+
+
