5f9b5ca0cfe40804992b40145f376fcb5f9faad5
Lesedi <leseditumelo32@gmail.com>
2021-06-13 20:31:00 +0200

    make your own coin

--- chap10/coins.py
+++ chap10/coins.py
@@ -1,17 +1,35 @@
 #project 9
-#class is a templates
+#A class is a template
+#rare is a boolean,true or false
 class Pound:
+	def __init__(self,rare):
 
-	
-	value = 1.00
-	colour = "gold"
-	num_edges = 1
-	diameter = 22.5 #mm
-	thickness = 3.15 #mm
-	heads = True 
+		self.rare = rare
 
-#object are instants 
-coin1 = Pound()
-print(coin1.value)
+		if self.rare:
+			self.value = 1.25
+		else:
+			self.value = 1.00
+
+		self.value = 1.00
+		self.colour = "gold"
+		self.num_edges = 1
+		self.diameter = 22.5 #mm
+		self.thickness = 3.15 #mm
+		self.heads = True
+
+	def rust(self):
+		self.colour = "greenish"
+
+	def clean(self):
+		self.colour = "gold"
+
+	def flip(self):
+		heads_options = [True, False]
+		choice = random.choice(heads_options)
+		self.heads = choice
+
+
+
 
 
