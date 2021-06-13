de2e5dac11acc908fbc3fb1f9b1105efb8cfbbc9
Lesedi <leseditumelo32@gmail.com>
2021-06-13 19:58:18 +0200

    OOP in python

---
+++ chap10/coins.py
@@ -0,0 +1,16 @@
+#project 9
+#class is a templates
+class Pound:
+
+	value = 1.00
+	colour = "gold"
+	num_edges = 1
+	diameter = 22.5 #mm
+	thickness = 3.15 #mm
+	heads = True 
+
+#object are instants 
+coin1 = Pound()
+print(coin1.value)
+
+
