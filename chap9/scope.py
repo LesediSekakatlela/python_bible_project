1330b9dd3d0f742d0c636037574b1d85c925fe51
Lesedi <leseditumelo32@gmail.com>
2021-06-13 18:12:00 +0200

    global variables

--- chap9/scope.py
+++ chap9/scope.py
@@ -1,3 +1,20 @@
+#part 1
+#global and local variables
+a = 250
+
+def f1():
+    b = 250 +10
+    print(b)
+
+def f2():
+    a = 50 
+    print(a)
+
+f1()
+f2()
+print(a)
+
+#part 2
 a = [1,2,3]
 
 def f1():
@@ -11,4 +28,5 @@ def f2():
 f1()
 f2()
 print(a)
+
 
